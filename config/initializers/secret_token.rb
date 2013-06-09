6f0bd8e16942e2a4bc0c5351cdb39f66572b0769
Andy Short <andynshort@gmail.com>
2013-06-09 14:24:55 -0700

    set up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svgsweeper::Application.config.secret_token = 'ae158e6b04a89867cfe5004fd753820b806bc68f4eab0ce42e3ff6306147cbfd68d20002f385ee61c50f9e95580d500f426d3adbd3468637afe9b04445948e04'
